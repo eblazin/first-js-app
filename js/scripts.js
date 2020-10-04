7803f62ab68e82fd4b70818b166f7afe2b21aeed
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-03 23:22:19 -0400

    Created new variable and assigned array
    Created variable 'pokemonlist' and assigned 3 array items.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: "Blastoise", height: "1.6m", types: ["Grass", "Electric", "Steel"] },
+  { name: "Sandslash", height: "1.0m", types: ["Grass", "Water", "Ice"] },
+  { name: "Pichu", height: "0.3m", types: ["Ground", "Flying", "Steel"] },
+];
