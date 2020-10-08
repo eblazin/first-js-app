32d06e6cec7d48eb159406e73bfa5ca35953b612
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-08 10:46:23 -0400

    Updated js file to use const and index method for array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-let pokemonList = [
-  { name: "Blastoise", height: "1.6m", types: ["Grass", "Electric", "Steel"] },
-  { name: "Sandslash", height: "1.0m", types: ["Grass", "Water", "Ice"] },
-  { name: "Pichu", height: "0.3m", types: ["Ground", "Flying", "Steel"] },
-];
+//Using Const inlieu of let because Const prevents the value from being changed through reassignment
+const pokemonList = [];
+pokemonList[0] = {
+  name: "Blastoise",
+  height: "1.6m",
+  types: ["Grass", "Electric", "Steel"]
+};
+pokemonList[1] = {
+  name: "Sandslash", height: "1.0m", types: ["Grass", "Water", "Ice"]
+};
+pokemonList[2] = {
+  name: "Pichu", height: "0.3m", types: ["Ground", "Flying", "Steel"]
+};
+
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(name);
+}
