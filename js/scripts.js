bc029c0aa63f512bfcd8e232bc6b1b0e9d43d134
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-09 11:02:26 -0400

    removed units from height value

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 //Using Const inlieu of let because Const prevents the value from being changed through reassignment
 const pokemonList = [];
+//Adding Pokemon to the pokemonList array
 pokemonList[0] = {
   name: "Blastoise",
-  height: "1.6m",
+  height: "1.6",
   types: ["Grass", "Electric", "Steel"]
 };
 pokemonList[1] = {
-  name: "Sandslash", height: "1.0m", types: ["Grass", "Water", "Ice"]
+  name: "Sandslash",
+  height: "1.0",
+  types: ["Grass", "Water", "Ice"]
 };
 pokemonList[2] = {
-  name: "Pichu", height: "0.3m", types: ["Ground", "Flying", "Steel"]
+  name: "Pichu",
+  height: "0.3",
+  types: ["Ground", "Flying", "Steel"]
 };
 
-
-
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(name);
+//creating loop to write name and height of all items in pokemonList array
+for (let i = 0; i <= pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')  ');
 }
