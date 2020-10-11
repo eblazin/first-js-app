5f9b34dbcfc044521245294d67466bddbbfe9eae
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-10 23:20:04 -0400

    added additional CSS and made site more responsive.

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,6 @@ for (let i = 0; i <= pokemonList.length; i++) {
   if (pokemonList[i].height > 1.0) {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - Wow, that\'s big!' + "<br>" + "<br>");
   } else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + "<br>");
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + "<br>" + "<br>");
   }
 }
