b8ce61cd1c5019ed3f713349137fe79e7c3edba8
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-18 13:42:17 -0400

    Refactored code to use forEach

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ pokemonList.push({
 
 //creating loop to write name and height of all items in pokemonList array
 //added conditional printing "wow, that's big" if height exceeds 1.0. 
+/*
 for (let i = 0; i <= pokemonList.length; i++) {
   if (pokemonList[i].height > 1.0) {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - Wow, that\'s big!' + "<br>" + "<br>");
@@ -26,3 +27,13 @@ for (let i = 0; i <= pokemonList.length; i++) {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + "<br>" + "<br>");
   }
 }
+*/
+
+//refactoring code commented out above to use forEach() to iterate over repository array
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height > 1.0) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ' + ' - Wow, that\'s big!' + "<br>" + "<br>");
+  } else {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ' + "<br>" + "<br>");
+  }
+})
