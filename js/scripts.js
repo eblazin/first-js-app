1054f1424bbedcba0be580d1fdc74bd1474054f9
eblazin <48164274+eblazin@users.noreply.github.com>
2020-10-18 13:51:40 -0400

    Replaced String Literals with Template Literals

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,8 @@ for (let i = 0; i <= pokemonList.length; i++) {
 //refactoring code commented out above to use forEach() to iterate over repository array
 pokemonList.forEach(function (pokemon) {
   if (pokemon.height > 1.0) {
-    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ' + ' - Wow, that\'s big!' + "<br>" + "<br>");
+    document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, that's big! <br> <br>`);
   } else {
-    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ' + "<br>" + "<br>");
+    document.write(`${pokemon.name} (height: ${pokemon.height})<br> <br>`);
   }
 })
