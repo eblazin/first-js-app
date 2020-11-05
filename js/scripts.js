bd0e1f1b907c0de1a461a0430bb5059e09f97546
flittle <48164274+eblazin@users.noreply.github.com>
2020-11-05 14:47:13 -0500

    delete showModal to replace with bootstrap javascript

--- js/scripts.js
+++ js/scripts.js
@@ -20,57 +20,13 @@ function showDetails(pokemon){
     let modalContainer = document.querySelector('#modal-container');
 
     function showModal(title, text){
-      modalContainer.innerHTML = '';
-
-      let modal = document.createElement('div');
-      modal.classList.add('modal');
-
-      //Add the new modal content
-      let closeButtonElement = document.createElement('button');
-      closeButtonElement.classList.add('modal-close');
-      closeButtonElement.innerText = 'X';
-      closeButtonElement.addEventListener('click', hideModal);
-
-      window.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-          hideModal();  
-        }
-      });
-
-      modalContainer.addEventListener('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal container,
-        // We only want to close if the user clicks directly on the overlay
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-      });
-      
-
-      let titleElement = document.createElement('h1');
-      titleElement.innerText = title; //pokemon name
-
-      let contentElement = document.createElement('p');
-      contentElement.innerText = text;
-
-      let imageElement = document.createElement('img');
-      imageElement.src = pokemon.imageUrl;
-    
-
-      modal.appendChild(closeButtonElement);
-      modal.appendChild(titleElement);
-      modal.appendChild(contentElement);
-      modal.appendChild(imageElement);
-      modalContainer.appendChild(modal);
-
-      modalContainer.classList.add('is-visible');
-    }
+      modal
 
     function hideModal(){
       modalContainer.classList.remove('is-visible');
     }
-    
-      showModal(pokemon);
+  
+    showModal(pokemon);
     
 
 
