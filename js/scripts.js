bfe4d009958f18cd4f25e6aef68958eb1ff656f1
flittle <48164274+eblazin@users.noreply.github.com>
2020-11-08 14:19:45 -0500

    removed spaces

--- js/scripts.js
+++ js/scripts.js
@@ -3,59 +3,57 @@
 const pokemonRepository = (function () {
   const pokemonList = [];
   //fetching the api with the list of Pokemon
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+
   //Defining functions to add items to the pokemonList and to return all items in the pokemonList array
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
-  
-  function getAll(){
+
+  function getAll() {
     return pokemonList;
   }
   //changing show details function to loaddetails from the pokemon api
-  function showDetails(pokemon){
-    loadDetails(pokemon).then(function (){
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
       //adding Modal functionality
-        function showModal(item) {
+      function showModal(item) {
+        let modalBody = $(".modal-body");
+        let modalTitle = $(".modal-title");
 
-          let modalBody = $(".modal-body");
-          let modalTitle = $(".modal-title");
-          
-          modalTitle.empty(); //empty the title - this is important as they may add up
-          modalBody.empty();  //empty the body - this is important as they may add up
-          
-          let nameElement = $("<h1>" + item.name + "</h1>");
-          //Creating an image element
-          let imageElementFront = $('<img class="modal-img" style="width:50%">');
-          imageElementFront.attr("src", item.imageUrl);
-          let imageElementBack = $('<img class="modal-img" style="width:50%">');
-          imageElementBack.attr("src", item.imageUrlBack);
-          
-          //creating element for height in modal content
-          let heightElement = $("<p> " + "height : " + item.height + "</p>");
-          //creating element for weight in modal content
-          let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
-          
-          modalTitle.append(nameElement);
-          modalBody.append(imageElementFront);
-          modalBody.append(imageElementBack);
-          modalBody.append(heightElement);
-          modalBody.append(weightElement);
-          
-          }
+        modalTitle.empty(); //empty the title - this is important as they may add up
+        modalBody.empty(); //empty the body - this is important as they may add up
 
-          showModal(pokemon);
-        });
-        }
-          
-        //modalContainer.innerHTML = '';
-  
-        //let modal = document.createElement('div');
-        //modal.classList.add('modal');
-  
-        //Old Modal Content
-        /*
+        let nameElement = $("<h1>" + item.name + "</h1>");
+        //Creating an image element
+        let imageElementFront = $('<img class="modal-img" style="width:50%">');
+        imageElementFront.attr("src", item.imageUrl);
+        let imageElementBack = $('<img class="modal-img" style="width:50%">');
+        imageElementBack.attr("src", item.imageUrlBack);
+
+        //creating element for height in modal content
+        let heightElement = $("<p> " + "height : " + item.height + "</p>");
+        //creating element for weight in modal content
+        let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
+
+        modalTitle.append(nameElement);
+        modalBody.append(imageElementFront);
+        modalBody.append(imageElementBack);
+        modalBody.append(heightElement);
+        modalBody.append(weightElement);
+      }
+
+      showModal(pokemon);
+    });
+  }
+
+  //modalContainer.innerHTML = '';
+
+  //let modal = document.createElement('div');
+  //modal.classList.add('modal');
+
+  //Old Modal Content
+  /*
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
         closeButtonElement.innerText = 'X';
@@ -95,9 +93,7 @@ const pokemonRepository = (function () {
   
         modalContainer.classList.add('is-visible');
         */
- 
-  
-  
+
   function addListItem(pokemon) {
     let unorderedList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
@@ -105,65 +101,72 @@ const pokemonRepository = (function () {
     button.innerText = pokemon.name;
     button.classList.add("item-button", "btn", "btn-primary");
     button.setAttribute("data-toggle", "modal");
-    button.setAttribute("data-target", "#modal-container")
+    button.setAttribute("data-target", "#modal-container");
     listpokemon.classList.add("list-group-item");
     listpokemon.appendChild(button);
     unorderedList.appendChild(listpokemon);
-    button.addEventListener('click', ()=>{
-      showDetails(pokemon)});//adding event listener to button, and using an arrow function to callthe showDetails function
+    button.addEventListener("click", () => {
+      showDetails(pokemon);
+    }); //adding event listener to button, and using an arrow function to callthe showDetails function
   }
   //adding LoadList() method to fetch data from the API, then adding Pokemon to pokemonList
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch((e) => {
+        console.error(e);
       });
-    }).catch(e => {
-      console.error(e);
-    })
   }
   //loading the detailed data for a given Pokemon
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.imageUrlBack = details.sprites.back_default;
-      item.height = details.height;
-      item.weight = details.weight
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.imageUrlBack = details.sprites.back_default;
+        item.height = details.height;
+        item.weight = details.weight;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
-  // returning objects with add, getAll, and addListItem as keys 
+  // returning objects with add, getAll, and addListItem as keys
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
   };
-  })();
-  
-  console.log(pokemonRepository.getAll());
-  
-  pokemonRepository.loadList().then(function() {
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
-  });
-  //creating loop to write name and height of all items in pokemonList array
-  //added conditional printing "wow, that's big" if height exceeds 1.0. 
-  
-  /*
+});
+//creating loop to write name and height of all items in pokemonList array
+//added conditional printing "wow, that's big" if height exceeds 1.0.
+
+/*
   for (let i = 0; i <= pokemonList.length; i++) {
     if (pokemonList[i].height > 1.0) {
       document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - Wow, that\'s big!' + "<br>" + "<br>");
@@ -172,11 +175,10 @@ const pokemonRepository = (function () {
     }
   }
   */
-  //refactoring code commented out above to use forEach() to iterate over repository array
-  //Replaced String Literals with Template Literals
-  
-  
-    /* if (pokemon.height > 1.0) {
+//refactoring code commented out above to use forEach() to iterate over repository array
+//Replaced String Literals with Template Literals
+
+/* if (pokemon.height > 1.0) {
       document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, that's big! <br> <br>`);
     } else {
       document.write(`${pokemon.name} (height: ${pokemon.height})<br> <br>`);
